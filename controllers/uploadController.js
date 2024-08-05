a28fae1543ee43df8690a317ea567cf910b22df1
GFHaaify <gustavo.fitipaldi@haaify.com>
2024-08-05 11:53:27 -0300

    fix

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,4 +1,4 @@
-const { formatarData, formatarHorario } = require('../utils/utils');
+const { formatarData, formatarHorario } = require('../utils/format');
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const { v4: uuidv4 } = require('uuid');
 const fetch = (...args) =>
